877d5ea1c7711a806893b938863407d53ce107e5
xeladu <xeladu@outlook.de>
2022-05-22 00:17:02 +0200

    Improve demo UI

--- lib/firebase/auth.dart
+++ lib/firebase/auth.dart
@@ -3,14 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 class Auth {
   static Future<String?> mailRegister(String mail, String pwd) async {
     try {
-      var res = await FirebaseAuth.instance
+      await FirebaseAuth.instance
           .createUserWithEmailAndPassword(email: mail, password: pwd);
-      print("Mail: ${res.user?.email}");
-      print("Display Name: ${res.user?.displayName}");
-      print("User Name: ${res.additionalUserInfo?.username}");
-      print("Method: ${res.credential?.signInMethod}");
-      print("Token: ${res.credential?.token}");
-      print("User UID: ${res.user?.uid}");
       return null;
     } on FirebaseAuthException catch (ex) {
       return "${ex.code}: ${ex.message}";
@@ -28,14 +22,8 @@ class Auth {
 
   static Future<String?> mailSignIn(String mail, String pwd) async {
     try {
-      var res = await FirebaseAuth.instance
+      await FirebaseAuth.instance
           .signInWithEmailAndPassword(email: mail, password: pwd);
-      print("Mail: ${res.user?.email}");
-      print("Display Name: ${res.user?.displayName}");
-      print("User Name: ${res.additionalUserInfo?.username}");
-      print("Method: ${res.credential?.signInMethod}");
-      print("Token: ${res.credential?.token}");
-      print("User UID: ${res.user?.uid}");
       return null;
     } on FirebaseAuthException catch (ex) {
       return "${ex.code}: ${ex.message}";
@@ -44,14 +32,7 @@ class Auth {
 
   static Future<String?> googleSignIn() async {
     try {
-      var res =
-          await FirebaseAuth.instance.signInWithPopup(GoogleAuthProvider());
-      print("Mail: ${res.user?.email}");
-      print("Display Name: ${res.user?.displayName}");
-      print("User Name: ${res.additionalUserInfo?.username}");
-      print("Method: ${res.credential?.signInMethod}");
-      print("Token: ${res.credential?.token}");
-      print("User UID: ${res.user?.uid}");
+      await FirebaseAuth.instance.signInWithPopup(GoogleAuthProvider());
       return null;
     } on FirebaseAuthException catch (ex) {
       return "${ex.code}: ${ex.message}";
--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,5 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_firebase/firebase/auth.dart';
 
 class AuthPage extends StatefulWidget {
@@ -12,108 +12,159 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   String _pwd = "";
   String _mail = "";
+  User? _user;
+
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseAuth.instance.authStateChanges().listen((user) {
+      setState(() {
+        _user = user;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Firebase Auth")),
       body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ListView(children: [
-          Card(
-            color: Colors.grey.shade200,
-            child: Column(children: [
-              Container(height: 10),
-              const Text("Email and password",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-              Container(height: 10),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: TextField(
-                    onChanged: (value) {
-                      setState(() {
-                        _mail = value;
-                      });
-                    },
-                    decoration: const InputDecoration(label: Text("Email"))),
+          padding: const EdgeInsets.all(10.0),
+          child: Stack(children: [
+            ListView(children: [
+              Card(
+                color: Colors.grey.shade200,
+                child: Column(children: [
+                  Container(height: 10),
+                  const Text("Email and password",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  Container(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: TextField(
+                        onChanged: (value) {
+                          setState(() {
+                            _mail = value;
+                          });
+                        },
+                        decoration:
+                            const InputDecoration(label: Text("Email"))),
+                  ),
+                  Container(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: TextField(
+                        onChanged: (value) {
+                          setState(() {
+                            _pwd = value;
+                          });
+                        },
+                        decoration:
+                            const InputDecoration(label: Text("Password"))),
+                  ),
+                  Container(height: 10),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () async {
+                              var res = await Auth.mailRegister(_mail, _pwd);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                      backgroundColor: res == null
+                                          ? Colors.green
+                                          : Colors.red,
+                                      content: Text(res ?? "Registered!")));
+                            },
+                            child: const Text("Register")),
+                        ElevatedButton(
+                            onPressed: () async {
+                              var res = await Auth.mailSignIn(_mail, _pwd);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                      backgroundColor: res == null
+                                          ? Colors.green
+                                          : Colors.red,
+                                      content: Text(res ?? "Logged in!")));
+                            },
+                            child: const Text("Login"))
+                      ]),
+                  Container(height: 10)
+                ]),
               ),
               Container(height: 10),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: TextField(
-                    onChanged: (value) {
-                      setState(() {
-                        _pwd = value;
-                      });
-                    },
-                    decoration: const InputDecoration(label: Text("Password"))),
+              Card(
+                color: Colors.grey.shade200,
+                child: Column(children: [
+                  Container(height: 10),
+                  const Text("Login with Google",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  Container(height: 10),
+                  ElevatedButton(
+                      onPressed: () async {
+                        var res = await Auth.googleSignIn();
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            backgroundColor:
+                                res == null ? Colors.green : Colors.red,
+                            content: Text(res ?? "Logged in!")));
+                      },
+                      child: const Text("Login with Google")),
+                  Container(height: 10)
+                ]),
               ),
               Container(height: 10),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-                ElevatedButton(
-                    onPressed: () async {
-                      var res = await Auth.mailRegister(_mail, _pwd);
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          backgroundColor:
-                              res == null ? Colors.green : Colors.red,
-                          content: Text(res ?? "Registered!")));
-                    },
-                    child: const Text("Register")),
-                ElevatedButton(
-                    onPressed: () async {
-                      var res = await Auth.mailSignIn(_mail, _pwd);
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          backgroundColor:
-                              res == null ? Colors.green : Colors.red,
-                          content: Text(res ?? "Logged in!")));
-                    },
-                    child: const Text("Login"))
-              ]),
-              Container(height: 10)
+              Card(
+                color: Colors.grey.shade200,
+                child: Column(children: [
+                  Container(height: 10),
+                  const Text("Log out",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  Container(height: 10),
+                  ElevatedButton(
+                      onPressed: () async {
+                        var res = await Auth.signOut();
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            backgroundColor:
+                                res == null ? Colors.green : Colors.red,
+                            content: Text(res ?? "Logged out!")));
+                      },
+                      child: const Text("Sign out")),
+                  Container(height: 10)
+                ]),
+              ),
+              Container(height: 10),
+              if (_user != null)
+                Card(
+                    color: Colors.grey.shade200,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text("User data",
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            Container(height: 10),
+                            Text("Mail: ${_user?.email}"),
+                            Text("Display Name: ${_user?.displayName}"),
+                            Text("User UID: ${_user?.uid}")
+                          ]),
+                    ))
             ]),
-          ),
-          Container(height: 10),
-          Card(
-            color: Colors.grey.shade200,
-            child: Column(children: [
-              Container(height: 10),
-              const Text("Login with Google",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-              Container(height: 10),
-              ElevatedButton(
-                  onPressed: () async {
-                    var res = await Auth.googleSignIn();
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        backgroundColor:
-                            res == null ? Colors.green : Colors.red,
-                        content: Text(res ?? "Logged in!")));
-                  },
-                  child: const Text("Login with Google")),
-              Container(height: 10)
-            ]),
-          ),
-          Container(height: 10),
-          Card(
-            color: Colors.grey.shade200,
-            child: Column(children: [
-              Container(height: 10),
-              const Text("Log out",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-              Container(height: 10),
-              ElevatedButton(
-                  onPressed: () async {
-                    var res = await Auth.signOut();
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        backgroundColor:
-                            res == null ? Colors.green : Colors.red,
-                        content: Text(res ?? "Logged out!")));
-                  },
-                  child: const Text("Sign out")),
-              Container(height: 10)
-            ]),
-          )
-        ]),
-      ),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  _user != null ? "Logged in" : "Logged out",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: _user != null ? Colors.green : Colors.red,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ))
+          ])),
     );
   }
 }
